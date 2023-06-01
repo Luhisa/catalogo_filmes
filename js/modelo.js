38f97c80501b07480332fbccf749ccc03cecb91d
Luhisa <luisavtrsantos@gmail.com>
2023-06-01 08:17:17 -0300

    first commit

---
+++ js/modelo.js
@@ -0,0 +1,44 @@
+let modeloJson = [
+    {
+      id: 1,
+      name: 'Piratas do Caribe: A Maldição do Pérola Negra',
+      img: 'img/pc_perolanegra.png',
+      description: 'Data de lançamento: 29 de agosto de 2003 (Brasil)',
+      link: 'https://youtu.be/yVUjeU9I3u8',
+    },
+    {
+      id: 2,
+      name: 'Piratas do Caribe: O Baú da Morte',
+      img: 'img/pc_bau.png',
+      description: 'Data de lançamento: 21 de julho de 2006 (Brasil)',
+      link:'https://youtu.be/IY4P1I_0zL4',
+    },
+    {
+      id: 3,
+      name: 'Piratas do Caribe: No Fim do Mundo',
+      img: 'img/pc_nofimdomundo.png',
+      description: 'Data de lançamento: 25 de maio de 2007 (Brasil)',
+      link: 'https://youtu.be/gpWKgrWPft0',
+    },
+    {
+      id: 4,
+      name: 'Piratas do Caribe: Navegando em Águas Misteriosas',
+      img: 'img/pc_aguasmisteriosas.png',
+      description: 'Data de lançamento: 7 de maio de 2011 (EUA)',
+      link:'https://youtu.be/haenBC_xPnc',
+    },
+    {
+      id: 5,
+      name: 'Piratas do Caribe: A Vingança de Salazar',
+      img: 'img/pc_vinganca.png',
+      description: 'Data de lançamento: 25 de maio de 2017 (Brasil)',
+      link: 'https://youtu.be/z3DK__4q648',
+    },
+    {
+      id: 6,
+      name: 'Saiba mais sobre a franquia!!',
+      img: 'img/pc_saibamais.png',
+      description: 'clique na imagem',
+      link: 'https://www.legiaodosherois.com.br/lista/10-coisas-que-talvez-voce-nao-saiba-sobre-piratas-do-caribe.html#list-item-1',
+    },
+  ];
